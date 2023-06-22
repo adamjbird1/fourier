6c7d515b2ba8bb003faae04e22ae70daed8b476f
Adam Bird <adamjbird1@gmail.com>
2023-06-22 20:05:39 +0100

    feat: add epicontroller

---
+++ src/ts/epi-controller.ts
@@ -0,0 +1,121 @@
+import CanvasController from "./canvas-controller";
+import { Point, Complex, FourierCoef } from "./common-types";
+import { dft } from "./fourier";
+
+export class EpiController extends CanvasController {
+
+    rawPoints: Array<Point> = [];
+    path: Array<Point> = [];
+    fourierCoeffs: Array<FourierCoef> = [];
+    animProgress: number = 0;
+    period: number = 10;
+    pathChanged: boolean = false;
+    coeffCount = 5;
+
+    constructor(id) {
+        super(id);
+    }
+
+    setPath(points: Array<Point>) {
+        this.rawPoints = points.slice();
+        this.fourierCoeffs = dft(points.map(p => new Complex(p.x, p.y)));
+        this.fourierCoeffs.sort((a, b) => b.amp - a.amp);
+        this.pathChanged = true;
+    }
+
+    updatePath() {
+        let pathProgress = 0;
+
+        this.path = [];
+
+        for (let i = 0; i < this.rawPoints.length; i++) {
+            pathProgress += 1/this.rawPoints.length;
+            this.addPoint(pathProgress);
+        }
+        console.log(this.path)
+    }
+
+    addPoint(progress) {
+        let currentX = 0;
+        let currentY = 0;
+
+        for (let i = 0; i < this.fourierCoeffs.length; i++) {
+            const amplitude = this.fourierCoeffs[i].amp;
+            const angle = 2 * Math.PI * this.fourierCoeffs[i].freq * progress + this.fourierCoeffs[i].phase;
+
+            if (i > this.coeffCount) {
+                continue
+            }
+
+            currentX += amplitude * Math.cos(angle);
+            currentY += amplitude * Math.sin(angle);
+
+            // if (i == 0 || amplitude < 0.5) {
+            //     continue;
+            // }
+        }
+        this.path.push({x: currentX, y: currentY});
+    }
+
+    drawPath() {
+        this.clear();
+    
+        if (this.path.length < 2) {
+            return;
+        }
+    
+        this.ctxt.beginPath();
+        this.ctxt.moveTo(this.path[0].x, this.path[0].y);
+    
+        for (let i = 1; i < this.path.length; i++) {
+            this.ctxt.lineTo(this.path[i].x, this.path[i].y);
+        }
+
+        // since the drawing must be a loop
+        this.ctxt.lineTo(this.path[0].x, this.path[0].y);
+    
+        this.ctxt.stroke();
+    }
+
+    drawCircles() {
+        let currentX = 0;
+        let currentY = 0;
+
+        for (let i = 0; i < this.fourierCoeffs.length; i++) {
+            const amplitude = this.fourierCoeffs[i].amp;
+            const angle = 2 * Math.PI * this.fourierCoeffs[i].freq * this.animProgress + this.fourierCoeffs[i].phase;
+            currentX += amplitude * Math.cos(angle);
+            currentY += amplitude * Math.sin(angle);
+
+            if (i == 0 || i > this.coeffCount || amplitude < 0.5) {
+                continue;
+            }
+
+            // if (i > 3) {
+            //     continue;
+            // }
+
+            this.ctxt.beginPath();
+            this.ctxt.lineWidth = 1;
+            this.ctxt.moveTo(currentX, currentY);
+            this.ctxt.arc(currentX, currentY, amplitude, angle - Math.PI, angle + Math.PI);
+            this.ctxt.stroke();
+                
+        }
+    }
+
+    update(dt, _) {
+        this.animProgress += dt/this.period;
+        this.animProgress = this.animProgress % 1.0;
+
+        if (this.pathChanged) {
+            this.updatePath();
+            this.pathChanged = false;
+        }
+    }
+    
+    render() {
+        this.drawPath();
+        this.drawCircles();
+    }
+}
