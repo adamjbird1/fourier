55723961dc865782fffaa3624f9208ecfdf41129
Adam Bird <adamjbird1@gmail.com>
2023-06-22 20:06:00 +0100

    feat: add main.ts, link code to page

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,9 +1,17 @@
 import { Conductor } from "./conductor";
 import { DrawController } from "./draw-controller";
+import { EpiController } from "./epi-controller";
 
 function init() {
-    let sketchController = new DrawController('drawingCanvas')
-    let conductor = new Conductor([sketchController]);
+    let sketchController = new DrawController('drawingCanvas');
+    let epiController = new EpiController('renderCanvas');
+
+    // register callbacks for when drawing is done
+
+    sketchController.onDrawingStart.push(() => epiController.setPath([]));
+    sketchController.onDrawingEnd.push(() => epiController.setPath(sketchController.path));
+
+    let conductor = new Conductor([sketchController, epiController]);
     conductor.start();
 }
 
