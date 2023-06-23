1abaa35d3155a64b95481a8b6c0ca4df0331d97f
Adam Bird <adamjbird1@gmail.com>
2023-06-23 15:13:59 +0100

    fix: Slider updates correctly

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,38 +1,41 @@
 import { Conductor } from "./conductor";
 import { DrawController } from "./draw-controller";
 import { EpiController } from "./epi-controller";
+import { WaveController } from "./waves-controller";
 
 function init() {
     let sketchController = new DrawController('drawingCanvas');
     let epiController = new EpiController('renderCanvas');
+    let wavesController = new WaveController('wavesCanvas', null);
+
+    wavesController.setWave();
+
+    sketchController.onDrawingStart.push(() => epiController.setPath([]));
+    sketchController.onDrawingEnd.push(() => epiController.setPath(sketchController.path));
+
+    // create slider
 
     let pageCoeffCount = document.getElementById('coeffCount');
-
     let slider = <HTMLInputElement>document.getElementById("slider");
 
     let sliderChange = () => {
         epiController.setCoeffCount(slider.value);
+        console.log(epiController.coeffCount);
         pageCoeffCount!.innerHTML = String(epiController.coeffCount);
     }
 
-    // TODO: change from onchange so that it updates as slider is dragged
-
     if (slider !== null) {
-        slider.onchange = sliderChange;
-    }
-
-    
+        slider.oninput = sliderChange;
+    }    
 
     // register callbacks 
-    sketchController.onDrawingStart.push(() => epiController.setPath([]));
-    sketchController.onDrawingStart.push(() => {
+    
+    sketchController.onDrawingEnd.push(() => {
         slider.value = String(1);
         sliderChange();
-    })
-    sketchController.onDrawingEnd.push(() => epiController.setPath(sketchController.path));
-
+    })  
     
-    let conductor = new Conductor([sketchController, epiController]);
+    let conductor = new Conductor([sketchController, epiController, wavesController]);
     conductor.start();
 }
 
